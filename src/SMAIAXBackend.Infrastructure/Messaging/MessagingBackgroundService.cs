d33f59db997167e2dd2dc86d1e327b17693bd9d2
Essiga <Adrian.Essig@outlook.com>
2024-10-15 10:56:59 +0200

    feat(messaging): added hivemqtestcontainer

--- src/SMAIAXBackend.Infrastructure/Messaging/MessagingBackgroundService.cs
+++ src/SMAIAXBackend.Infrastructure/Messaging/MessagingBackgroundService.cs
@@ -5,13 +5,11 @@ namespace SMAIAXBackend.Infrastructure.Messaging;
 
 public class MessagingBackgroundService(IMqttReader mqttReader) : BackgroundService
 {
-    private readonly IMqttReader _mqttReader = mqttReader;
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         try
         {
-            await _mqttReader.ConnectAndSubscribeAsync();
+            await mqttReader.ConnectAndSubscribeAsync();
 
             // Keep the service running until the token is canceled
             while (!stoppingToken.IsCancellationRequested)
@@ -22,7 +20,7 @@ public class MessagingBackgroundService(IMqttReader mqttReader) : BackgroundServ
         }
         finally
         {
-            await _mqttReader.DisconnectAsync();
+            await mqttReader.DisconnectAsync();
         }
     }
 }
--- tests/SMAIAXBackend.IntegrationTests/IntegrationTestSetup.cs
+++ tests/SMAIAXBackend.IntegrationTests/IntegrationTestSetup.cs
@@ -1,3 +1,5 @@
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
 using Microsoft.Extensions.DependencyInjection;
 using SMAIAXBackend.Infrastructure.DbContexts;
 using Testcontainers.PostgreSql;
@@ -8,6 +10,7 @@ namespace SMAIAXBackend.IntegrationTests;
 internal static class IntegrationTestSetup
 {
     private static PostgreSqlContainer _postgresContainer = null!;
+    private static IContainer _hiveMqContainer = null!;
     private static WebAppFactory _webAppFactory = null!;
     public static ApplicationDbContext ApplicationDbContext { get; private set; } = null!;
     public static HttpClient HttpClient { get; private set; } = null!;
@@ -24,9 +27,17 @@ internal static class IntegrationTestSetup
             .Build();
 
         await _postgresContainer.StartAsync();
+        
+        _hiveMqContainer = new ContainerBuilder()
+            .WithImage("hivemq/hivemq4")
+            .WithPortBinding(1883, true)
+            .Build();
 
-        var connectionString = _postgresContainer.GetConnectionString();
-        _webAppFactory = new WebAppFactory(connectionString);
+        await _hiveMqContainer.StartAsync();
+        
+        var postgresConnectionString = _postgresContainer.GetConnectionString();
+        var hiveMqPort = _hiveMqContainer.GetMappedPublicPort(1883);
+        _webAppFactory = new WebAppFactory(postgresConnectionString, hiveMqPort);
 
         HttpClient = _webAppFactory.CreateClient();
 
