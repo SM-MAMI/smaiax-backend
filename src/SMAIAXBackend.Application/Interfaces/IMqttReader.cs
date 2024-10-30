318bc1cf7d22189fa0b865cb949c9ec8195ec3e6
Michael Spiegel <michaelspiegel@live.at>
2024-10-30 20:38:30 +0100

    chore: remove unused IMqttReader.cs

--- src/SMAIAXBackend.Application/Interfaces/IMqttReader.cs
+++
@@ -1,8 +0,0 @@
-namespace SMAIAXBackend.Application.Interfaces;
-
-// TODO: Remove this interface
-public interface IMqttReader
-{
-    Task ConnectAndSubscribeAsync();
-    Task DisconnectAsync();
-}
