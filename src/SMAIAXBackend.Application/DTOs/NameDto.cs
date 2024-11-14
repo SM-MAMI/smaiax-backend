118aed63a5ac96eb954543091847a40336795315
Marco Prescher <mpr8243@students.fhv.at>
2024-11-14 10:58:57 +0100

    feat(api-validation): add missing 'required' annotations to DTO fields

--- src/SMAIAXBackend.Application/DTOs/NameDto.cs
+++ src/SMAIAXBackend.Application/DTOs/NameDto.cs
@@ -5,8 +5,10 @@ namespace SMAIAXBackend.Application.DTOs;
 public class NameDto(string firstName, string lastName)
 {
     [Required]
+    [RegularExpression(@"^\S+$", ErrorMessage = "First name cannot be empty or whitespace")]
     public string FirstName { get; set; } = firstName;
 
     [Required]
+    [RegularExpression(@"^\S+$", ErrorMessage = "Last name cannot be empty or whitespace")]
     public string LastName { get; set; } = lastName;
 }
