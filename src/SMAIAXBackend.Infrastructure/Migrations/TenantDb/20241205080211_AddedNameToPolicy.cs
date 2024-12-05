f76d73a96f203bf9c07f23dd9633daf82542f95f
Essiga <Adrian.Essig@outlook.com>
2024-12-05 09:08:09 +0100

    feat(policy-overview): added name database migraton

---
+++ src/SMAIAXBackend.Infrastructure/Migrations/TenantDb/20241205080211_AddedNameToPolicy.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMAIAXBackend.Infrastructure.Migrations.TenantDb
+{
+    /// <inheritdoc />
+    public partial class AddedNameToPolicy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                schema: "domain",
+                table: "Policy",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                schema: "domain",
+                table: "Policy");
+        }
+    }
+}
