08dc5af027f7bc3ba2debcc6864269117e5135b9
Essiga <Adrian.Essig@outlook.com>
2024-12-12 10:58:56 +0100

    fix(policy-names): dotnet format

--- src/SMAIAXBackend.Application/Exceptions/PolicyNameMissingException.cs
+++ src/SMAIAXBackend.Application/Exceptions/PolicyNameMissingException.cs
@@ -1,6 +1,6 @@
 namespace SMAIAXBackend.Application.Exceptions;
 
-public class PolicyNameMissingException :  Exception
+public class PolicyNameMissingException : Exception
 {
     public override string Message { get; } = "Policy Name missing";
 }
